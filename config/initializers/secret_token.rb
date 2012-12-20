e94d218c8ee09cb92ba06245a459365081fe9601
Hari Vedam <hvedam@gmail.com>
2012-12-19 19:35:49 -0500

    Initial Checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scholarangel::Application.config.secret_token = '4c7728f43301b1e82ca9c4cadc84fa152a711207a87fc3abfbd4af4974ab4f167d51c7edc58c5b581580046121b636b79372cdd9322a54a0dada288f9f10ee70'
